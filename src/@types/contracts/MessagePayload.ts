1baa1ef51c60f9ba4d29431cad57e88a4574c3a4
ok-kioo <kaio.viana@upe.br>
2026-06-28 00:56:59 -0300

    fix: update MessagePayload to use string for apiPayload and adjust MessageService publish method accordingly

--- src/@types/contracts/MessagePayload.ts
+++ src/@types/contracts/MessagePayload.ts
@@ -1,10 +1,9 @@
 import { Payload } from "./PayloadBase";
-import { JsonValue } from "./JsonValue";
 
 export type MessagePayload = Payload & {
   kind: "MESSAGE_PAYLOAD";
   event: string;
   idempotencyKey: string;
-  apiPayload: JsonValue;
+  apiPayload: string;
   timestamp: string;
 };
